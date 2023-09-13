6b0554f6dbb5326ac63d515fccbf8043dbac10e4
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2023-09-13 18:11:57 -0400

    feat (component): membership card (#46)
    * build(config): new layouts
    
    * build(templates): completed templates
    
    * fix(components): move to new directory
    
    * feat(layout): new
    
    * feat(templates): new njk build
    
    * chore(components): remove
    
    * build(templates): add build files

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addLayoutAlias("blank", "layouts/blank.njk");
 	eleventyConfig.addLayoutAlias("index", "layouts/index.njk");
 	eleventyConfig.addLayoutAlias("base", "layouts/base.njk");
+	eleventyConfig.addLayoutAlias("newsletter", "layouts/newsletter.njk");
+	eleventyConfig.addLayoutAlias("component", "layouts/component.njk");
 
 	return {
 		htmlTemplateEngine: "njk",
