4677a08dd40c10fa303d59db961290dc56cc6229
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2024-02-13 19:17:01 -0500

    chore(data): moved to _data directory

---
+++ src/_data/affiliations/dc.js
@@ -0,0 +1,32 @@
+const data = {
+	address_1: "915 15th St. NW",
+	address_2: "Washington, D.C. 20005",
+	affiliate_name: "ACLU of the District of Columbia",
+	donation_href: "https://action.aclu.org/give/district-columbia-join-renew-today?initms_aff=dc&initms_chan=eml&utm_medium=eml&initms=22XXXX_affcampaign_footerdonate_gradead_sail&utm_source=sail&utm_campaign=affcampaign&utm_content=22XXXX_civilliberties_footerdonate_gradead&af={sb_query_string_encrypted}&ms_aff=dc&ms_chan=eml&ms=22XXXX_affcampaign_footerdonate_gradead_sail",
+	facebook_href: "https://www.facebook.com/aclu.dc/",
+	instagram_href: "https://www.instagram.com/acluofdc",
+	logo_url: "https://media.sailthru.com/64d/1k4/4/d/5e947c1762b9b.png",
+	logo_width: "150",
+	twitter_href: "https://twitter.com/ACLU_DC",
+	website: "https://www.acludc.org/",
+	signatures: [
+		{
+			ed: {
+				title: "Executive Director",
+				name: "Monica Hopkins",
+				pronouns: "She, her, hers",
+				headshot: "https://media.sailthru.com/64d/1k4/3/2/5e5d757ab2965.jpg",
+			},
+		},
+		{
+			advo: {
+				title: "Political Director",
+				name: "Nassim Moshiree",
+				pronouns: "She, her, hers",
+				headshot: "https://media.sailthru.com/64d/1k4/4/s/5ea8913ea14ef.png",
+			},
+		},
+	],
+};
+
+module.exports = data;
--- src/_data/affiliations/nat.js
+++ src/_data/affiliations/nat.js
@@ -5,9 +5,27 @@ const data = {
 	donation: "https://action.aclu.org/give/protect-rights-freedoms-we-believe-4-multistep?initms_aff=nat&initms_chan=eml&utm_medium=eml&initms=21XXXX_affcampaign_footerdonate_gradead_sail&utm_source=sail&utm_campaign=affcampaign&utm_content=21XXXX_civilliberties_footerdonate_gradead&af={sb_query_string_encrypted}&ms_aff=nat&ms_chan=eml&ms=21XXXX_affcampaign_footerdonate_gradead_sail",
 	facebook: "https://www.facebook.com/aclu",
 	instagram: "https://www.instagram.com/aclu_nationwide/",
-	logo: "https://media.sailthru.com/64d/1k3/a/o/5db1cbcdd662a.png",
+	logo_url: "https://media.sailthru.com/64d/1k3/a/o/5db1cbcdd662a.png",
 	twitter: "https://twitter.com/aclu",
 	website: "https://www.aclu.org/",
+	signatures: [
+		{
+			ed: {
+				title: "Executive Director",
+				name: "Anthony Romero",
+				pronouns: "He, him, his",
+				headshot: "https://media.sailthru.com/64d/1k3/a/o/5db2043ae79b8.jpg",
+			},
+		},
+		{
+			advo: {
+				title: "Political Director",
+				name: "Ronald Newman",
+				pronouns: "He, him, his",
+				headshot: "https://media.sailthru.com/64d/1k3/a/o/5db2052e87d81.jpg",
+			},
+		},
+	],
 };
 
 module.exports = data;
