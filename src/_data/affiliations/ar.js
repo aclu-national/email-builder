f79dd9ead09d6ca328aaf2de7c17635e09b8ef16
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2024-02-14 23:27:23 -0500

    feat(data): add arkansas and arizona (#52)

---
+++ src/_data/affiliations/ar.js
@@ -0,0 +1,80 @@
+const data = {
+	address_1: "904 West 2nd Street",
+	address_2: "Little Rock, AR 72201",
+	affiliate_name: "ACLU of Arkansas",
+	donation_href: "https://action.aclu.org/give/arkansas-join-renew-today?initms_aff=ar&initms_chan=eml&utm_medium=eml&initms=22XXXX_affcampaign_footerdonate_gradead_sail&utm_source=sail&utm_campaign=affcampaign&utm_content=22XXXX_civilliberties_footerdonate_gradead&af={sb_query_string_encrypted}&ms_aff=ar&ms_chan=eml&ms=22XXXX_affcampaign_footerdonate_gradead_sail",
+	facebook_href: "https://www.facebook.com/ACLUArkansas",
+	instagram_href: "https://www.instagram.com/acluofarkansas/",
+	logo_url: "https://media.sailthru.com/64d/1k8/2/9/65c67d6786f7c.png",
+	logo_width: "200",
+	twitter_href: "https://twitter.com/ArkansasACLU",
+	website: "https://www.acluarkansas.org",
+	signatures: [
+		{
+			ed: {
+				title: "Executive Director",
+				name: "Holly Dickson",
+				pronouns: "She, her, hers",
+				headshot: "https://media.sailthru.com/64d/1k3/b/8/5dc58b034237f.jpg",
+			},
+		},
+		{
+			advo: {
+				title: "Advocacy Director",
+				name: "",
+				pronouns: "",
+				headshot: "",
+			},
+		},
+		{
+			comms: {
+				title: "Chief Communications Officer",
+				name: "",
+				pronouns: "",
+				headshot: "",
+			},
+		},
+		{
+			dev: {
+				title: "Director of Development",
+				name: "",
+				pronouns: "",
+				headshot: "",
+			},
+		},
+		{
+			legal: {
+				title: "Legal Director",
+				name: "Holly Dickson",
+				pronouns: "She, her, hers",
+				headshot: "https://media.sailthru.com/64d/1k3/b/8/5dc58b034237f.jpg",
+			},
+		},
+		{
+			custom1: {
+				title: "",
+				name: "",
+				pronouns: "",
+				headshot: "",
+			},
+		},
+		{
+			custom2: {
+				title: "",
+				name: "",
+				pronouns: "",
+				headshot: "",
+			},
+		},
+		{
+			custom3: {
+				title: "",
+				name: "",
+				pronouns: "",
+				headshot: "",
+			},
+		},
+	],
+};
+
+module.exports = data;
---
+++ src/_data/affiliations/az.js
@@ -0,0 +1,80 @@
+const data = {
+	address_1: "P.O. Box 17148",
+	address_2: "Phoenix, AZ 85011",
+	affiliate_name: "ACLU of Arizona",
+	donation_href: "https://action.aclu.org/give/arizona-join-renew-today?initms_aff=az&initms_chan=eml&utm_medium=eml&initms=22XXXX_affcampaign_footerdonate_gradead_sail&utm_source=sail&utm_campaign=affcampaign&utm_content=22XXXX_civilliberties_footerdonate_gradead&af={sb_query_string_encrypted}&ms_aff=az&ms_chan=eml&ms=22XXXX_affcampaign_footerdonate_gradead_sail",
+	facebook_href: "https://www.facebook.com/ACLUofArizona",
+	instagram_href: "https://www.instagram.com/acluofarizona",
+	logo_url: "https://media.sailthru.com/64d/1k4/4/d/5e947c15c5459.png",
+	logo_width: "150",
+	twitter_href: "https://twitter.com/ACLUaz",
+	website: "https://www.acluaz.org",
+	signatures: [
+		{
+			ed: {
+				title: "Executive Director",
+				name: "",
+				pronouns: "",
+				headshot: "",
+			},
+		},
+		{
+			advo: {
+				title: "Advocacy & Legal Director",
+				name: "Victoria López",
+				pronouns: "",
+				headshot: "https://media.sailthru.com/64d/1k4/4/s/5ea8782bea764.jpg",
+			},
+		},
+		{
+			comms: {
+				title: "Communications Director",
+				name: "Marcela Taracena",
+				pronouns: "",
+				headshot: "https://media.sailthru.com/64d/1k4/4/s/5ea8782636b5f.jpg",
+			},
+		},
+		{
+			dev: {
+				title: "Director of Development",
+				name: "Logan Marshall",
+				pronouns: "",
+				headshot: "",
+			},
+		},
+		{
+			legal: {
+				title: "",
+				name: "",
+				pronouns: "",
+				headshot: "",
+			},
+		},
+		{
+			custom1: {
+				title: "Policy Director",
+				name: "Darrell Hill",
+				pronouns: "",
+				headshot: "https://media.sailthru.com/64d/1k4/4/s/5ea8783354f37.png",
+			},
+		},
+		{
+			custom2: {
+				title: "",
+				name: "",
+				pronouns: "",
+				headshot: "",
+			},
+		},
+		{
+			custom3: {
+				title: "",
+				name: "",
+				pronouns: "",
+				headshot: "",
+			},
+		},
+	],
+};
+
+module.exports = data;
