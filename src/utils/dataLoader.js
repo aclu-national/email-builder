d76170aca6b048f42f5d3348cbd0b3ada91843a5
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2024-02-14 22:29:30 -0500

    feat(data): dataLoader (#50)
    * feat(templates): add can testing template
    
    * chore(config): increased line length
    
    * feat(data): output dataLoader file to dist directory
    
    * chore(config): remove husky folder
    
    * chore(data): moved to _data directory
    
    * chore(data): no longer needed
    
    * feat(data): creates a data file for sailthru
    
    * feat(partials): update var names to remove affiliates to make more generic
    
    * chore(custom): utils
    
    hide clg output
    
    * build(data): add to build command
    
    hide clg output
    
    * chore(config): ignore sailthru data file since it does not match true json
    
    hide clg output

---
+++ src/utils/dataLoader.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const path = require("path");
+const nat = require("../_data/affiliations/nat");
+const dc = require("../_data/affiliations/dc");
+const variables = require("./variables");
+
+const combinedData = {
+	dc,
+	nat,
+};
+
+const minifiedJsonString = "{data = " + JSON.stringify(combinedData) + "}";
+
+const distDir = path.resolve(__dirname, "..", "..", "dist", "data");
+const minifiedDistPath = path.join(distDir, "dataLoader.min.json");
+
+// Ensure the directory exists
+fs.mkdirSync(distDir, { recursive: true });
+
+let data = (minifiedJsonString + variables).replace(/\s/g, "");
+fs.writeFile(minifiedDistPath, minifiedJsonString + variables, (err) => {
+	if (err) throw err;
+	console.log("Minified data written to file");
+});
+
+// console.log(data);
