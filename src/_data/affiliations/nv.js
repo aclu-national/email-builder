817eb6ef3e2a10d1627afce67a7615c35f86b4bd
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2024-05-20 17:39:26 -0400

    feat(data): create all affiliation files

---
+++ src/_data/affiliations/nv.js
@@ -0,0 +1,64 @@
+const data = {
+	address1: "",
+	address2: "",
+	affiliation_name: "",
+	donation_url: "",
+	facebook_url: "",
+	instagram_url: "",
+	logo_url: "",
+	logo_width: "150",
+	signatures: {
+		advo: {
+			title: "Chief Political and Advocacy Officer",
+			name: "",
+			pronouns: "",
+			headshot: "",
+		},
+		comms: {
+			title: "Chief Communications and Marketing Officer",
+			name: "",
+			pronouns: "",
+			headshot: "",
+		},
+		custom1: {
+			title: "",
+			name: "",
+			pronouns: "",
+			headshot: "",
+		},
+		custom2: {
+			title: "",
+			name: "",
+			pronouns: "",
+			headshot: "",
+		},
+		custom3: {
+			title: "",
+			name: "",
+			pronouns: "",
+			headshot: "",
+		},
+		dev: {
+			title: "Chief Development Officer",
+			name: "",
+			pronouns: "",
+			headshot: "",
+		},
+		ed: {
+			title: "Executive Director",
+			name: "",
+			pronouns: "",
+			headshot: "",
+		},
+		legal: {
+			title: "Legal Director",
+			name: "",
+			pronouns: "",
+			headshot: "",
+		},
+	},
+	twitter_url: "",
+	website_url: "",
+};
+
+module.exports = data;
