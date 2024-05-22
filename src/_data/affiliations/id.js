440d9116739adea24b725912a58f8764755ec619
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2024-05-22 19:15:39 -0400

    chore(data): update f-l

--- src/_data/affiliations/id.js
+++ src/_data/affiliations/id.js
@@ -1,64 +1,70 @@
 const data = {
-	address1: "",
-	address2: "",
-	affiliation_name: "",
-	donation_url: "",
-	facebook_url: "",
-	instagram_url: "",
-	logo_url: "",
+	address1: "P.O. Box 1897",
+	address2: "Boise, ID 83701",
+	affiliate_name: "ACLU of Idaho",
+	donation_url: "https://action.aclu.org/give/idaho-join-renew-today?initms_aff=id&initms_chan=eml&utm_medium=eml&initms=22XXXX_affcampaign_footerdonate_gradead_sail&utm_source=sail&utm_campaign=affcampaign&utm_content=22XXXX_civilliberties_footerdonate_gradead&af={sb_query_string_encrypted}&ms_aff=id&ms_chan=eml&ms=22XXXX_affcampaign_footerdonate_gradead_sail",
+	facebook_url: "https://www.facebook.com/ACLUIdaho",
+	instagram_url: "https://www.instagram.com/acluidaho",
+	logo_url: "https://media.sailthru.com/64d/1k4/4/d/5e947c184d612.png",
 	logo_width: "150",
 	signatures: {
 		advo: {
+			headshot: "https://www.acluidaho.org/sites/default/files/styles/featured_image_580x386/public/field_image/image.jpg?itok=EPdzqnPA", //TODO: create headshot and save it to ST.
+			name: "Julianne Donnelly Tzul",
+			pronouns: "She, her, hers",
 			title: "Chief Political and Advocacy Officer",
-			name: "",
-			pronouns: "",
-			headshot: "",
 		},
 		comms: {
-			title: "Chief Communications and Marketing Officer",
-			name: "",
-			pronouns: "",
-			headshot: "",
+			headshot: "https://www.acluidaho.org/sites/default/files/styles/featured_image_580x386/public/field_image/rebecca_web.jpg?itok=tO5cgrCg", //TODO: create headshot and save it to ST.
+			name: "Rebecca De León",
+			pronouns: "She, any",
+			title: "Communications Director",
 		},
 		custom1: {
-			title: "",
+			headshot: "",
 			name: "",
 			pronouns: "",
-			headshot: "",
+			title: "",
 		},
 		custom2: {
-			title: "",
+			headshot: "",
 			name: "",
 			pronouns: "",
-			headshot: "",
+			title: "",
 		},
 		custom3: {
-			title: "",
+			headshot: "",
 			name: "",
 			pronouns: "",
-			headshot: "",
+			title: "",
 		},
 		dev: {
-			title: "Chief Development Officer",
-			name: "",
-			pronouns: "",
-			headshot: "",
+			headshot: "https://www.acluidaho.org/sites/default/files/styles/featured_image_580x386/public/field_image/kdillon_1.jpg?itok=QkOlKLym", //TODO: create headshot and save it to ST.
+			name: "Kelsey Dillon",
+			pronouns: "She, her, hers",
+			title: "Director of Philanthropy",
 		},
 		ed: {
+			headshot: "https://www.acluidaho.org/sites/default/files/styles/featured_image_580x386/public/field_image/lmorales_1.jpg?itok=puMkt2Ou", //TODO: create headshot and save it to ST.
+			name: "Leo Morales",
+			pronouns: "He, him, his",
 			title: "Executive Director",
-			name: "",
-			pronouns: "",
-			headshot: "",
 		},
 		legal: {
+			headshot: "https://www.acluidaho.org/sites/default/files/styles/featured_image_580x386/public/field_image/paul_web.jpg?itok=s22jBOtv", //TODO: create headshot and save it to ST.
+			name: "Paul Carlos Southwick",
+			pronouns: "He, him, his",
 			title: "Legal Director",
+		},
+		org: {
+			headshot: "",
 			name: "",
 			pronouns: "",
-			headshot: "",
+			title: "Organizing Director",
 		},
 	},
-	twitter_url: "",
-	website_url: "",
+	twitter_url: "https://twitter.com/acluidaho",
+	website_url: "https://www.acluidaho.org",
 };
 
 module.exports = data;
--- src/_data/affiliations/ky.js
+++ src/_data/affiliations/ky.js
@@ -1,64 +1,70 @@
 const data = {
-	address1: "",
-	address2: "",
-	affiliation_name: "",
-	donation_url: "",
-	facebook_url: "",
-	instagram_url: "",
-	logo_url: "",
+	address1: "315 Guthrie Street, Suite 300",
+	address2: "Louisville, KY 40202",
+	affiliate_name: "ACLU of Kentucky",
+	donation_url: "https://action.aclu.org/give/kentucky-join-renew-today?initms_aff=ky&initms_chan=eml&utm_medium=eml&initms=22XXXX_affcampaign_footerdonate_gradead_sail&utm_source=sail&utm_campaign=affcampaign&utm_content=22XXXX_civilliberties_footerdonate_gradead&af={sb_query_string_encrypted}&ms_aff=ky&ms_chan=eml&ms=22XXXX_affcampaign_footerdonate_gradead_sail",
+	facebook_url: "https://www.facebook.com/ACLUofKY",
+	instagram_url: "https://www.instagram.com/aclu_nationwide",
+	logo_url: "https://media.sailthru.com/64d/1k4/4/d/5e947c1962c57.png",
 	logo_width: "150",
 	signatures: {
 		advo: {
-			title: "Chief Political and Advocacy Officer",
-			name: "",
-			pronouns: "",
-			headshot: "",
+			headshot: "https://www.aclu-ky.org/sites/default/files/styles/featured_image_580x386/public/field_image/kate_miller.jpg?itok=f-7XffVq", //TODO: create headshot and save it to ST.
+			name: "Kate Miller",
+			pronouns: "She, her, hers",
+			title: "Advocacy Director",
 		},
 		comms: {
-			title: "Chief Communications and Marketing Officer",
-			name: "",
-			pronouns: "",
-			headshot: "",
+			headshot: "https://www.aclu-ky.org/sites/default/files/styles/featured_image_580x386/public/field_image/ac_headshot_01.png?itok=5lH7HQ5x", //TODO: create headshot and save it to ST.
+			name: "Angela Cooper",
+			pronouns: "She, her, hers",
+			title: "Communications Director",
 		},
 		custom1: {
-			title: "",
+			headshot: "",
 			name: "",
 			pronouns: "",
-			headshot: "",
+			title: "",
 		},
 		custom2: {
-			title: "",
+			headshot: "",
 			name: "",
 			pronouns: "",
-			headshot: "",
+			title: "",
 		},
 		custom3: {
-			title: "",
+			headshot: "",
 			name: "",
 			pronouns: "",
-			headshot: "",
+			title: "",
 		},
 		dev: {
-			title: "Chief Development Officer",
-			name: "",
-			pronouns: "",
-			headshot: "",
+			headshot: "https://www.aclu-ky.org/sites/default/files/styles/featured_image_580x386/public/field_image/charlene_buckles_2.jpg?itok=5_TbEiFe", //TODO: create headshot and save it to ST.
+			name: "Charlene Buckles",
+			pronouns: "She, her, hers",
+			title: "Development Director",
 		},
 		ed: {
+			headshot: "https://www.aclu-ky.org/sites/default/files/styles/featured_image_580x386/public/field_image/amber_duke.jpg?itok=H-Y9ElU5", //TODO: create headshot and save it to ST.
+			name: "Amber Duke",
+			pronouns: "She, her, hers",
 			title: "Executive Director",
-			name: "",
-			pronouns: "",
-			headshot: "",
 		},
 		legal: {
+			headshot: "https://www.aclu-ky.org/sites/default/files/styles/featured_image_580x386/public/field_image/corey_shapiro_0.jpg?itok=vDI35wS_", //TODO: create headshot and save it to ST.
+			name: "Corey Shapiro",
+			pronouns: "He, him, his",
 			title: "Legal Director",
+		},
+		org: {
+			headshot: "",
 			name: "",
 			pronouns: "",
-			headshot: "",
+			title: "Organizing Director",
 		},
 	},
-	twitter_url: "",
-	website_url: "",
+	twitter_url: "https://twitter.com/ACLUofKY",
+	website_url: "https://www.aclu-ky.org",
 };
 
 module.exports = data;
